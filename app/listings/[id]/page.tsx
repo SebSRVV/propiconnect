910995932c0db1b0618fb4d0c8184b6a1b3aa071
SebRVV <112523636+SebSRVV@users.noreply.github.com>
2025-05-10 12:19:17 -0500

    feat: agregar páginas para eliminar y editar propiedades, y proporcionar un contexto de sesión para la autenticación

--- app/listings/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import PropertyPageContent from './PageContent';
-
-export default async function Page({ params }: { params: { id: string } }) {
-  return <PropertyPageContent propiedadID={params.id} />;
-}
