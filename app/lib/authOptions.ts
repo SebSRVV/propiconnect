51b48502a0d9a920f50011e435bfd8dc30cffc78
SebRVV <112523636+SebSRVV@users.noreply.github.com>
2025-05-10 00:14:30 -0500

    feat: mover opciones de autenticación a un archivo separado para mejorar la organización del código

---
+++ app/lib/authOptions.ts
@@ -0,0 +1,70 @@
+// lib/authOptions.ts
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: 'Credenciales',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Contraseña', type: 'password' },
+      },
+      async authorize(credentials) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+
+        if (!email || !password) return null;
+
+        const credencial = await prisma.credencial.findUnique({
+          where: { email },
+          include: { usuario: true },
+        });
+
+        const usuario = credencial?.usuario;
+
+        if (!credencial || !usuario || !credencial.password) {
+          return null;
+        }
+
+        const passwordMatch = await bcrypt.compare(password, credencial.password);
+        if (!passwordMatch) return null;
+
+        return {
+          id: String(usuario.userID),
+          name: `${usuario.nombres} ${usuario.apellidos}`,
+          email: credencial.email,
+          tipoUsuario: usuario.tipoUsuario,
+        };
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.userID = user.id;
+        token.tipoUsuario = user.tipoUsuario;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token && session.user) {
+        session.user.userID = token.userID;
+        session.user.tipoUsuario = token.tipoUsuario;
+      }
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/login',
+  },
+};
