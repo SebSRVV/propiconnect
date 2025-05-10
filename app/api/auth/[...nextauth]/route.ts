766e4ccd836d48e1e31aab42638093db7a8d80c0
SebRVV <112523636+SebSRVV@users.noreply.github.com>
2025-05-10 00:19:56 -0500

    refactor: actualizar importación de authOptions para mejorar la estructura del proyecto

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
-// app/api/auth/[...nextauth]/route.ts
 import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/authOptions';
+import { authOptions } from '@/lib/authOptions'; // o usa ruta relativa si el alias no está configurado
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
